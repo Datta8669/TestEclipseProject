8b0d504b7ee8b9982365c3d3272ec029487f1339
Datta <Datta@Test.com>
2025-11-29 22:27:51 +0530

    added new file

--- villageinfo/src/main/java/com/Villageinfo/villageinfo/VillageinfoApplication.java
+++ villageinfo/src/main/java/com/Villageinfo/villageinfo/VillageinfoApplication.java
@@ -2,11 +2,13 @@ package com.Villageinfo.villageinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.SystemPropertyUtils;
 
 @SpringBootApplication
 public class VillageinfoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("bosssssssssssssssssssssss");
 		SpringApplication.run(VillageinfoApplication.class, args);
 	}
 
